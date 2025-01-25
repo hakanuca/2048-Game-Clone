7a89018273905da510e72aae3c98448c41239a68
HakanUca <hakanhuca@gmail.com>
2025-01-26 02:00:30 +0300

    new calculation locig for game

--- 2048 Clone/Assets/Scripts/Cell2048.cs
+++ 2048 Clone/Assets/Scripts/Cell2048.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,285 @@ public class Cell2048 : MonoBehaviour
     public Cell2048 left;
     public  Cell2048 down;
     public Cell2048 up;
-    // Start is called before the first frame update
-    void Start()
+
+    public Fill2048 fill;
+
+    private void OnEnable()
     {
-        
+        GameController2048.slide += OnSlide;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        GameController2048.slide -= OnSlide;
+
+    } 
+    private void OnSlide(string whatWasSent)
+    {
+        Debug.Log(whatWasSent);
+        if (whatWasSent == "w")
+        {
+            if (up != null)
+            {
+                return;
+            }
+
+            Cell2048 currentCell = this;
+            SlideUp(currentCell);
+        }
+        if (whatWasSent == "d")
+        {
+            if (right != null)
+            {
+                return;
+            }
+
+            Cell2048 currentCell = this;
+            SlideRight(currentCell);
+        }
+        if (whatWasSent == "s")
+        {
+            if (down != null)
+            {
+                return;
+            }
+
+            Cell2048 currentCell = this;
+            SlideDown(currentCell);
+        }
+        if (whatWasSent == "a")
+        {
+            if (left != null)
+            {
+                return;
+            }
+
+            Cell2048 currentCell = this;
+            SlideLeft(currentCell);
+        }
+    }
+
+    void SlideUp(Cell2048 currentCell)
+    {
+        if (currentCell.down == null)
+        {
+            return;
+        }
+        Debug.Log(currentCell.gameObject);
+        if (currentCell.fill != null)
+        {
+            Cell2048 nextCell = currentCell.down;
+            while (nextCell.down != null || nextCell.fill ==null)
+            {
+                nextCell = nextCell.down;
+            }
+
+            if (nextCell.fill != null)
+            {
+                if (currentCell.fill.value == nextCell.fill.value)
+                {
+                    Debug.Log("doubled");
+                    nextCell.fill.transform.parent = currentCell.transform;
+                    currentCell.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+                else
+                {
+                    Debug.Log("undoubled");
+                    nextCell.fill.transform.parent = currentCell.down.transform;
+                    currentCell.down.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+            }
+        }
+        else
+        {
+            Cell2048 nextCell = currentCell.down;
+            while (nextCell.down != null && nextCell.fill ==null)
+            {
+                nextCell = nextCell.down;
+            }
+            if (nextCell.fill != null)
+            {
+                nextCell.fill.transform.parent = currentCell.transform;
+                currentCell.fill = nextCell.fill;
+                nextCell.fill = null;
+                SlideUp(currentCell);
+                Debug.Log("slide to empty");
+            }
+        }
+        if (currentCell.down == null)
+        {
+            return;
+        }
+        SlideUp(currentCell.down);
+    }
+    
+    void SlideRight(Cell2048 currentCell)
+    {
+        if (currentCell.left == null)
+        {
+            return;
+        }
+        Debug.Log(currentCell.gameObject);
+        if (currentCell.fill != null)
+        {
+            Cell2048 nextCell = currentCell.left;
+            while (nextCell.left != null || nextCell.fill ==null)
+            {
+                nextCell = nextCell.left;
+            }
+
+            if (nextCell.fill != null)
+            {
+                if (currentCell.fill.value == nextCell.fill.value)
+                {
+                    Debug.Log("doubled");
+                    nextCell.fill.transform.parent = currentCell.transform;
+                    currentCell.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+                else
+                {
+                    Debug.Log("undoubled");
+                    nextCell.fill.transform.parent = currentCell.left.transform;
+                    currentCell.left.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+            }
+        }
+        else
+        {
+            Cell2048 nextCell = currentCell.left;
+            while (nextCell.left != null && nextCell.fill ==null)
+            {
+                nextCell = nextCell.left;
+            }
+            if (nextCell.fill != null)
+            {
+                nextCell.fill.transform.parent = currentCell.transform;
+                currentCell.fill = nextCell.fill;
+                nextCell.fill = null;
+                SlideRight(currentCell);
+                Debug.Log("slide to empty");
+            }
+        }
+        if (currentCell.left == null)
+        {
+            return;
+        }
+        SlideRight(currentCell.left);
+    }
+    
+    void SlideDown(Cell2048 currentCell)
+    {
+        if (currentCell.up == null)
+        {
+            return;
+        }
+        Debug.Log(currentCell.gameObject);
+        if (currentCell.fill != null)
+        {
+            Cell2048 nextCell = currentCell.up;
+            while (nextCell.up != null || nextCell.fill ==null)
+            {
+                nextCell = nextCell.up;
+            }
+
+            if (nextCell.fill != null)
+            {
+                if (currentCell.fill.value == nextCell.fill.value)
+                {
+                    Debug.Log("doubled");
+                    nextCell.fill.transform.parent = currentCell.transform;
+                    currentCell.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+                else
+                {
+                    Debug.Log("undoubled");
+                    nextCell.fill.transform.parent = currentCell.up.transform;
+                    currentCell.up.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+            }
+        }
+        else
+        {
+            Cell2048 nextCell = currentCell.up;
+            while (nextCell.up != null && nextCell.fill ==null)
+            {
+                nextCell = nextCell.up;
+            }
+            if (nextCell.fill != null)
+            {
+                nextCell.fill.transform.parent = currentCell.transform;
+                currentCell.fill = nextCell.fill;
+                nextCell.fill = null;
+                SlideDown(currentCell);
+                Debug.Log("slide to empty");
+            }
+        }
+        if (currentCell.up == null)
+        {
+            return;
+        }
+        SlideDown(currentCell.up);
+    }
+    
+    void SlideLeft(Cell2048 currentCell)
+    {
+        if (currentCell.right == null)
+        {
+            return;
+        }
+        Debug.Log(currentCell.gameObject);
+        if (currentCell.fill != null)
+        {
+            Cell2048 nextCell = currentCell.right;
+            while (nextCell.right != null || nextCell.fill ==null)
+            {
+                nextCell = nextCell.right;
+            }
+
+            if (nextCell.fill != null)
+            {
+                if (currentCell.fill.value == nextCell.fill.value)
+                {
+                    Debug.Log("doubled");
+                    nextCell.fill.transform.parent = currentCell.transform;
+                    currentCell.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+                else
+                {
+                    Debug.Log("undoubled");
+                    nextCell.fill.transform.parent = currentCell.right.transform;
+                    currentCell.right.fill = nextCell.fill;
+                    nextCell.fill = null;
+                }
+            }
+        }
+        else
+        {
+            Cell2048 nextCell = currentCell.right;
+            while (nextCell.right != null && nextCell.fill ==null)
+            {
+                nextCell = nextCell.right;
+            }
+            if (nextCell.fill != null)
+            {
+                nextCell.fill.transform.parent = currentCell.transform;
+                currentCell.fill = nextCell.fill;
+                nextCell.fill = null;
+                SlideLeft(currentCell);
+                Debug.Log("slide to empty");
+            }
+        }
+        if (currentCell.right == null)
+        {
+            return;
+        }
+        SlideLeft(currentCell.right);
     }
 }
--- 2048 Clone/Assets/Scripts/GameController2048.cs
+++ 2048 Clone/Assets/Scripts/GameController2048.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameController2048 : MonoBehaviour
 {
     [SerializeField] GameObject fillPrefab;
 
     [SerializeField] private Transform[] allCells;
-    // Start is called before the first frame update
+
+    public static Action<string> slide;
     void Start()
     {
         
@@ -20,18 +22,38 @@ public class GameController2048 : MonoBehaviour
         {
             SpawnFill();
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            slide("w");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            slide("d");
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            slide("s");
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            slide("a");
+
+        }
     }
 
     public void SpawnFill()
     {
-        int whichSpawn = Random.Range(0, allCells.Length);
+        int whichSpawn = UnityEngine.Random.Range(0, allCells.Length);
         if (allCells[whichSpawn].childCount != 0)
         {
             Debug.Log(allCells[whichSpawn].name +"is already spawned");
             SpawnFill();
             return;
         }
-        float chance = Random.Range(0f, 1f);
+        float chance = UnityEngine.Random.Range(0f, 1f);
         Debug.Log(chance);
         if (chance < .2f)
         {
@@ -42,6 +64,7 @@ public class GameController2048 : MonoBehaviour
             GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
             Debug.Log(2);
             Fill2048 temoFillComp = tempFill.GetComponent<Fill2048>();
+            allCells[whichSpawn].GetComponent<Cell2048>().fill = temoFillComp;
             temoFillComp.FillValueUpdate(2);
         }
         else
@@ -49,6 +72,7 @@ public class GameController2048 : MonoBehaviour
             GameObject tempFill = Instantiate(fillPrefab, allCells[whichSpawn]);
             Debug.Log(4);
             Fill2048 temoFillComp = tempFill.GetComponent<Fill2048>();
+            allCells[whichSpawn].GetComponent<Cell2048>().fill = temoFillComp;
             temoFillComp.FillValueUpdate(4);
         }
     }
