35e7f728af9de525a1270464676aa9ca68a5c291
HakanUca <hakanhuca@gmail.com>
2025-01-31 20:12:07 +0300

    new ui

--- 2048 Clone/Assets/Scripts/MainMenu.cs
+++ 2048 Clone/Assets/Scripts/MainMenu.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; // Required for scene management
 
-public class PlayButtonScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     // This method will be linked to the Play button
     public void OnPlayButtonClick()
@@ -11,5 +11,30 @@ public class PlayButtonScript : MonoBehaviour
         
         // Load the next scene by incrementing the index
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // This method will be linked to the Quit button
+    public void OnQuitButtonClick()
+    {
+        // Quit the application
+        Application.Quit();
+
+        // If running in the Unity Editor, stop playing
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // This method will be linked to the Credits button
+    public void OnCreditsButtonClick()
+    {
+        // Load the scene named "Credits"
+        SceneManager.LoadScene("Credits");
+    }
+    
+    public void OnBackButtonClick()
+    {
+        // Load the scene named "Credits"
+        SceneManager.LoadScene("MainMenu");
     }
 }
